f11c0f1bf963cf22730c04a36b16e1d0b3cffbea
KaranyaphatP <104723213+KaranyaphatP@users.noreply.github.com>
2022-05-01 22:17:52 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,6 +11,18 @@ let engine = false
 radio.setGroup(147)
 basic.showIcon(IconNames.Target)
 engine = false
+loops.everyInterval(500, function () {
+    if (engine == false) {
+        basic.showIcon(IconNames.No)
+        led.stopAnimation()
+    }
+})
+loops.everyInterval(500, function () {
+    if (engine == true) {
+        basic.showIcon(IconNames.Yes)
+        led.stopAnimation()
+    }
+})
 basic.forever(function () {
     radio.sendValue("mgX", input.acceleration(Dimension.X))
     radio.sendValue("mgY", input.acceleration(Dimension.Y))
